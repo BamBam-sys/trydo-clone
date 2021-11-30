7e96602a89927d55860c71ed3ce7bf1bd64e0059
BamBam-sys <lawalayobamidavid@gmail.com>
2021-11-30 01:01:18 +0100

    added funtionality for side nav

--- script.js
+++ script.js
@@ -18,6 +18,52 @@ $('.owl-carousel').owlCarousel({
   },
 });
 
+jQuery(document).ready(function ($) {
+  var alterClass = function () {
+    var ww = document.body.clientWidth;
+    if (ww < 964) {
+      $('.nav-listitem').addClass('disable');
+      $('.nav-dropdown li').addClass('disable');
+    } else if (ww >= 964) {
+      $('.nav-listitem').removeClass('disable');
+      $('.nav-dropdown li').removeClass('disable');
+    }
+  };
+  $(window).resize(function () {
+    alterClass();
+  });
+  //Fire it when the page first loads:
+  alterClass();
+});
+
+const navItems = document.querySelectorAll('.nav-listitem');
+
+function removeShow(arr, item) {
+  arr.forEach((element) => {
+    if (element !== item) {
+      element.classList.remove('show');
+    }
+  });
+}
+
+function removeRotate(arr, item) {
+  arr.forEach((element) => {
+    if (element !== item) {
+      element.classList.remove('rotate');
+    }
+  });
+}
+
+navItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    removeShow(navItems, item);
+    item.classList.toggle('show');
+
+    removeRotate(navItems, item);
+    item.classList.toggle('rotate');
+  });
+});
+
 const carouselSlide = document.querySelector('.slides');
 const slides = document.querySelectorAll('.slide');
 const thumbnails = document.querySelectorAll('.thumb');
@@ -53,8 +99,30 @@ const navSlide = () => {
   });
 };
 
+navSlide();
+
 const closeBtn = document.querySelector('.close-btn');
 
 closeBtn.addEventListener('click', () => nav.classList.remove('burger-active'));
 
-navSlide();
+//Get the button:
+mybutton = document.getElementById('top');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.transform = 'scale(1)';
+  } else {
+    mybutton.style.transform = 'scale(0)';
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
